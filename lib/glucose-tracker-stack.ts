b1a93500bd08dc63127df83158179b9293c94316
Gareth Brown <garethbrown82@gmail.com>
2025-02-13 15:26:01 +0000

    Initial commit with basic functionality

--- lib/glucose-tracker-stack.ts
+++ lib/glucose-tracker-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'node:path';
 
 export class GlucoseTrackerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const uploadFunction = new lambda.Function(this, 'FileUploadFunction', {
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GlucoseTrackerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const uploadApiGateway = new apiGateway.LambdaRestApi(this, 'FileUploadApiGateway', {
+      handler: uploadFunction,
+      restApiName: 'FileUploadRestApi',
+    })
   }
 }
