8ec20cf8cf0a1a1e28a5b41ffc1b253e57785c0b
Gareth Brown <garethbrown@Gareths-MacBook-Pro.local>
2025-02-13 11:48:35 +0000

    Initial commit

---
+++ test/glucose-tracker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GlucoseTracker from '../lib/glucose-tracker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/glucose-tracker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GlucoseTracker.GlucoseTrackerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
