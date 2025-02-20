8b03b35e48dcf5dc928f03790d3ba63b6a83018b
Gareth Brown <garethbrown82@gmail.com>
2025-02-20 16:31:23 +0000

    Update Lambda function to PUT item to DynamoDB table

--- lib/glucose-tracker-stack.ts
+++ lib/glucose-tracker-stack.ts
@@ -9,11 +9,20 @@ export class GlucoseTrackerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const insulinTrackTable = new dynamodb.TableV2(this, 'InsulinTrackTable', {
+      partitionKey: { name: 'timestamp', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
+
     const insulinTrackFunction = new lambda.Function(this, 'InsulinTrackFunction', {
       runtime: lambda.Runtime.NODEJS_LATEST,
       handler: 'index.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+      environment: {
+        INSULIN_TABLE: insulinTrackTable.tableName,
+      }
     });
+    insulinTrackTable.grantWriteData(insulinTrackFunction);
 
     const insulinTrackApiGateway = new apiGateway.LambdaRestApi(this, 'InsulinTrackApiGateway', {
       handler: insulinTrackFunction,
@@ -22,11 +31,5 @@ export class GlucoseTrackerStack extends cdk.Stack {
     });
     const injections = insulinTrackApiGateway.root.addResource('injections');
     injections.addMethod('POST');
-
-    const insulinTrackTable = new dynamodb.TableV2(this, 'InsulinTrackTable', {
-      partitionKey: { name: 'timestamp', type: dynamodb.AttributeType.STRING },
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    })
-    insulinTrackTable.grantReadData(insulinTrackFunction);
   }
 }
